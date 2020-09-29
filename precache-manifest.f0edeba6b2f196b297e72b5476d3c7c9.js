06cd41e78f8e9fb7647382d3c3deb73d8068c9a6
ganesh-yandigeri <ganesh.vgy@gmail.com>
2020-09-29 14:58:25 +0530

    Updates

---
+++ precache-manifest.f0edeba6b2f196b297e72b5476d3c7c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d241f46254c3cc7921010d83d021f063",
+    "url": "/nihani/index.html"
+  },
+  {
+    "revision": "ed97154d3a13a43e54c1",
+    "url": "/nihani/static/css/main.2205ee4e.chunk.css"
+  },
+  {
+    "revision": "de112d9dc5de4ea609d2",
+    "url": "/nihani/static/js/2.71282824.chunk.js"
+  },
+  {
+    "revision": "fa24d2988fd9ee29f9cba16979460922",
+    "url": "/nihani/static/js/2.71282824.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed97154d3a13a43e54c1",
+    "url": "/nihani/static/js/main.b4b35857.chunk.js"
+  },
+  {
+    "revision": "318844b171d389afe951",
+    "url": "/nihani/static/js/runtime-main.1bf4243a.js"
+  }
+]);
